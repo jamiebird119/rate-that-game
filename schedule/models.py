706e198f368bef2d332c9187868749ff05c8ee4a
jamiebird119 <jamie.bird119@gmail.com>
2020-11-26 14:44:52 +0000

    Added gamedata, schedule and teams models

--- schedule/models.py
+++ schedule/models.py
@@ -7,10 +7,30 @@ class Schedule(models.Model):
     id = models.CharField(primary_key=True, max_length=40)
     date = models.DateField()
     time = models.TimeField()
-    home = models.OneToOneField(
-        Team, to_field='name', related_name='+', on_delete=models.PROTECT)
-    away = models.OneToOneField(
-        Team, to_field='name', related_name='+', on_delete=models.PROTECT)
+    home = models.ForeignKey(
+        Team, to_field='name', related_name='home_matches', on_delete=models.PROTECT)
+    away = models.ForeignKey(
+        Team, to_field='name', related_name='away_matches', on_delete=models.PROTECT)
 
     def __str__(self):
         return self.id
+
+
+class GameData(models.Model):
+    scheduled = models.ForeignKey(Schedule, on_delete=models.PROTECT)
+    lead_changes = models.IntegerField()
+    teams_string = models.CharField(max_length=50, blank=False)
+    home_points = models.IntegerField()
+    away_points = models.IntegerField()
+    home_scoring = models.JSONField()
+    away_scoring = models.JSONField()
+    OT = models.BooleanField()
+    rating = models.DecimalField(decimal_places=1, max_digits=2, blank=True)
+
+    def get_rating(self):
+        self.rating = ((0.015 * (self.home_points + self.away_points)) - (0.01 * abs(
+            self.home_points - self.away_points)) + (0.06 * self.lead_changes))
+        self.save()
+
+    def __str__(self):
+        return self.teams_string
