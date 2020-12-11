7f22f5e28a7926fe3cc74f593090296b1253a8ee
jamiebird119 <jamie.bird119@gmail.com>
2020-12-11 15:05:31 +0000

    base url fix

--- rate_that_game/urls.py
+++ rate_that_game/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('admin/', admin.site.urls),
-    path('/', include('home.urls')),
+    path('', include('home.urls')),
     path('schedule/', include('schedule.urls')),
 ]
