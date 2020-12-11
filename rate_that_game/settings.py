a98a233d55874410992fa0106c1ef18f01740c74
jamiebird119 <jamie.bird119@gmail.com>
2020-12-11 15:21:39 +0000

    Media added to context processor

--- rate_that_game/settings.py
+++ rate_that_game/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -142,6 +143,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_LOCATION = 'media'
 
 API_KEY = os.environ.get("API_KEY")
 
