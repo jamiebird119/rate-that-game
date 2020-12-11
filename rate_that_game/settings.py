63ca947d952a4bc9e74162e8e85900ab074d8a04
jamiebird119 <jamie.bird119@gmail.com>
2020-12-11 14:34:38 +0000

    Added to heroku allowed hosts

--- rate_that_game/settings.py
+++ rate_that_game/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','rate-that-game.herokuapp.com/']
 
 
 # Application definition
