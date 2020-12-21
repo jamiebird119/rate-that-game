3044a9dec8e00468f63a29d91f7533b7e53a2ec3
jamiebird119 <jamie.bird119@gmail.com>
2020-12-21 15:51:35 +0000

    Edits to teams page

--- schedule/templatetags/formatdates.py
+++ schedule/templatetags/formatdates.py
@@ -13,3 +13,9 @@ def format_date(date, tag):
     elif tag == 'HEADER':
         date_new = date.strftime('%A %d %b')
     return date_new
+
+
+@register.filter(name="percentage")
+def percentage(perc):
+    value = round(perc*100)
+    return value
--- teams/views.py
+++ teams/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Team, Division, Conference
 from schedule.models import Schedule
+from datetime import datetime, timedelta, date
 
 # Create your views here.
 
@@ -28,20 +29,23 @@ def standings(request, search):
 def team_info(request, alias):
     team = Team.objects.get(alias=alias)
     template = 'teams/team.html'
+    today = date.today()
     home = []
-    away =[]
+    away = []
     for key in team.ratings_home.keys():
         scheduled = Schedule.objects.get(id=key)
         game_data = scheduled.gamedata_set.get()
-        home.append(
-            {id: game_data}
-        )
+        if scheduled.date >= today - timedelta(days=14):
+            home.append(
+                {id: game_data}
+            )
     for key in team.ratings_away.keys():
         scheduled = Schedule.objects.get(id=key)
         game_data = scheduled.gamedata_set.get()
-        away.append(
-            {id: game_data}
-        )
+        if scheduled.date >= today - timedelta(days=7):
+            away.append(
+                {id: game_data}
+            )
     games = {
         'home': home,
         'away': away,
