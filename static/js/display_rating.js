bf6e6a64c5ed5de1b6ebb44d3849dbec1e1f4bcb
jamiebird119 <jamie.bird119@gmail.com>
2021-01-06 14:07:30 +0000

    Edited fade in and out of ratings

--- static/js/display_rating.js
+++ static/js/display_rating.js
@@ -2,9 +2,11 @@
 function show_rating(id){
     $(`.loader-gif-container`).toggleClass("d-none")
     setTimeout(() => {
-        $(`#${id}`).toggleClass("d-none")
+    //    $(`#${id}`).toggleClass("d-none")
         $(`.loader-gif-container`).toggleClass("d-none")
-       }, 2000);
+       }, 1500);
+     setTimeout(() => {
+       $(`#${id}`).find(".rating").toggleClass("hide-rating show-rating")}, 1750);
     
 }
     
